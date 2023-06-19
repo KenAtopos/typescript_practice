a60b0e89e10fcbc293917c36dd2abbaa0de592f8
Ken Atopos <z1158229761@outlook.com>
2023-06-19 22:17:10 +0800

    add map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,3 +1,14 @@
+/// <reference types="@types/google.maps" />
 import { User } from "./User";
+import { Company } from "./Company";
 
 const user = new User();
+const company = new Company();
+
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
