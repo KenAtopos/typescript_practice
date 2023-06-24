8853eccb8bb69d2f6f24bd38f7ae8f5b31130e55
Ken Atopos <z1158229761@outlook.com>
2023-06-24 14:37:58 +0800

    implement the abstract class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,25 @@
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
 const nc = new NumbersCollection([10, 3, -5, 0]);
+const cc = new CharactersCollection("Xaayb");
 
-const sorter = new Sorter(nc);
-sorter.sort();
-console.log(nc.data);
+// const sorter = new Sorter(cc);
+// sorter.sort();
+// console.log(cc.data);
+
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
+
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+// linkedList.print();
+
+cc.sort();
+
+console.log(cc.data);
