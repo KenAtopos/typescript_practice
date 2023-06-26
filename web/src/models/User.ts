d9ff03eeedca1b68a1b7f2cc4ebda7028f1c9d2a
Ken Atopos <z1158229761@outlook.com>
2023-06-26 15:18:34 +0800

    optional interface property

---
+++ web/src/models/User.ts
@@ -0,0 +1,16 @@
+interface UserProps {
+  name?: string;
+  age?: number; // optional interface property
+}
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+}
