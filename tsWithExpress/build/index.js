ec32e56c87ddd7803ce668b4b31c39fd629e5717
Ken Atopos <z1158229761@outlook.com>
2023-07-05 02:08:13 +0800

    implement the advanced integration for ts with express

--- tsWithExpress/build/index.js
+++ tsWithExpress/build/index.js
@@ -3,14 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const loginRoutes_1 = require("./routes/loginRoutes");
-const cookie_session_1 = __importDefault(require("cookie-session"));
-const app = (0, express_1.default)();
+var express_1 = __importDefault(require("express"));
+var loginRoutes_1 = require("./routes/loginRoutes");
+var cookie_session_1 = __importDefault(require("cookie-session"));
+var controller_1 = require("./controllers/decorators/controller");
+require("./controllers/LoginController");
+var app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_session_1.default)({ keys: ["something"] }));
 app.use(loginRoutes_1.router);
-app.listen(3000, () => {
+app.use(controller_1.router);
+app.listen(3000, function () {
     console.log("listening on port 3000");
 });
