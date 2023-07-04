5314e591871e845682167174049004be4eb45977
Ken Atopos <z1158229761@outlook.com>
2023-07-04 16:15:45 +0800

    finish the decorator session

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,17 +1,42 @@
+@classDecorator
 class Boat {
-  color: string = 'red';
+  @testDecorator
+  color: string = "red";
 
+  @testDecorator
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
-  @testDecorator
-  pilot(): void {
-    console.log('swish');
+  @logErr("shit")
+  pilot(@parameterDecorator speed: string): void {
+    speed === "fast" ? console.log("slow down") : console.log("ok");
   }
 }
 
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
 function testDecorator(target: any, key: string): void {
-  console.log('Target:', target);
-  console.log('Key:', key);
+  console.log("Target:", target);
+  console.log("Key:", key);
+}
+
+function logErr(errMsg: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errMsg);
+      }
+    };
+  };
 }
