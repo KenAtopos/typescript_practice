63d11af8e393626e907850db8472f9de22e4cf08
Ken Atopos <z1158229761@outlook.com>
2023-07-06 23:40:51 +0800

    ts with redux

--- tsrr/src/index.tsx
+++ tsrr/src/index.tsx
@@ -10,9 +10,30 @@ const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
 
 class App extends React.Component<AppProps> {
+  state = { counter: 0 }; // must name as state
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
+
+// functional components
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>
+// }
 
 root.render(<App color="red" />);
